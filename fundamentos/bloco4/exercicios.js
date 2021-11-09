2a796381139623cbf54ebff0aa1d009c972a22a7
leticiaandrade <leticia933@gmail.com>
2021-11-09 19:36:14 -0300

    commit inicial

---
+++ fundamentos/bloco4/exercicios.js
@@ -0,0 +1,23 @@
+// 1. Faça um algoritmo que calcule a soma de 1 a 1000 usando a estrutura "for" e retorne no formato:
+// A soma de 1 a 1000 é: X.
+
+//2. Crie um algoritmo que conte quantos números do intervalo entre 2 e 150 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta.
+
+let divpor3 = 0;
+for (i=2; i>=150; i++){
+    if (i % 3 == 0) {
+        console.log(i)
+    }
+}
+
+//3. Crie um algoritmo que simula o jogo "pedra, papel e tesoura" levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
+// "Player 1 won" ou "A Ties" ou "Player 2 won". 
+
+//4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+//Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
+
+//5. Crie um algoritmo que recebe a idade de Carolzita , Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
+
+
